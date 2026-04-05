d7812941590f311d4b96e062dc2ef7c95cd6cf35
Jay Kharpuse <jaykharpuse51@gmail.com>
2026-04-05 21:18:15 +0530

    add frontend url

--- roadmap-website-frontend/src/helper/axiosInstance.ts
+++ roadmap-website-frontend/src/helper/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const apiBaseUrl =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_BASE_URL ||
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:8000";
 
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
